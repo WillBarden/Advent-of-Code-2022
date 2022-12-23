c81b3b86e8ec015ed3f2abe34a44ac12afc26490
Will Barden <wfbarden@gmail.com>
2022-12-23 01:02:15 -0500

    Day 5 complete

---
+++ day5/__init__.py
@@ -0,0 +1,68 @@
+from functools import reduce
+from math import ceil, floor
+import re
+from types import GeneratorType
+
+
+def get_input():
+  '''
+  Get input lines as a generator
+  '''
+  f = open('day5/input.txt', 'r')
+  
+  # Parse crates
+  crates = [[] for _ in range(9)]
+  line = f.readline().rstrip()
+  while len(line) == 35:
+    crate_values = [line[i] for i in list(range(1, 35, 4))]
+    for i, value in enumerate(crate_values):
+      if value != ' ':
+        crates[i].insert(0, value)
+    line = f.readline().rstrip()
+
+  # Move through space
+  f.readline()
+
+  # Parse moves
+  moves = []
+  line = f.readline().rstrip()
+  while line:
+    res = re.findall('^move (\d+) from (\d+) to (\d+)$', line)
+    res = tuple(int(r) for r in res[0])
+    res = (res[0], res[1] - 1, res[2] - 1)
+    moves.append(res)
+    line = f.readline().rstrip()
+
+  f.close()
+  return (crates, moves)
+
+
+def move_crate(crates, src, dst, moves=1):
+  for _ in range(moves):
+    crate = crates[src].pop()
+    crates[dst].append(crate)
+  return crates
+
+
+def last(values):
+  return values[len(values) - 1]
+
+
+def part1():
+  crates, moves = get_input()
+  crates = reduce(
+    lambda crates, move : move_crate(crates, move[1], move[2], move[0]),
+    moves,
+    crates
+  )
+  return ''.join([last(crate) for crate in crates])
+  
+
+def part2():
+  pass
+
+
+def run():
+  print('Day 5')
+  print(part1())
+  print(part2())
--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from day1 import run as run_day1
 from day2 import run as run_day2
 from day3 import run as run_day3
 from day4 import run as run_day4
+from day5 import run as run_day5
 
 def main():
   run_day1()
   run_day2()
   run_day3()
   run_day4()
+  run_day5()
 
 if __name__ == '__main__':
   main()
